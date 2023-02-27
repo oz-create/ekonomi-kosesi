ee53cfceb16ff0fd29a132574bac03d1928cba24
oz-create <ozlemengin4542@gmail.com>
2023-02-27 18:24:57 +0300

    iletisim sayfası bitti

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -91,3 +91,21 @@ const swiper = new Swiper('.swiper', {
   
   });
 
+//Form Error Messages
+document.getElementById("submitButton").addEventListener("click",()=>{
+  if(document.getElementById("name").value == ""){
+    document.getElementById("nameErrorMessage").style.display = "block";
+  }else{
+    document.getElementById("nameErrorMessage").style.display = "none";
+  }
+  if(document.getElementById("email").value == ""){
+    document.getElementById("emailErrorMessage").style.display = "block";
+  }else{
+    document.getElementById("emailErrorMessage").style.display = "none";
+  }
+  if(document.getElementById("subject").value == ""){
+    document.getElementById("subjectErrorMessage").style.display = "block";
+  }else{
+    document.getElementById("subjectErrorMessage").style.display = "none";
+  }
+})
